3f417363108867f2c911da06750478dd9fee232f
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2024-10-12 13:55:51 -0300

    Update python_package_manager.sh

--- install/tools/python_package_manager.sh
+++ install/tools/python_package_manager.sh
@@ -10,3 +10,7 @@ sudo pip install pip-autoremove
 sudo apt update
 sudo apt install pipx
 pipx ensurepath
+
+# install uv
+# https://docs.astral.sh/uv/
+curl -LsSf https://astral.sh/uv/install.sh | sh
