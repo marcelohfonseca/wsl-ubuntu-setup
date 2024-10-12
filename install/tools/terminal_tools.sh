7e2a320ad0608d8a8d5542308e8e95e1cfca1dcf
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2024-10-12 13:46:31 -0300

    Update terminal_tools.sh

--- install/tools/terminal_tools.sh
+++ install/tools/terminal_tools.sh
@@ -22,7 +22,3 @@ echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable
 sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
 sudo apt update
 sudo apt install -y eza
-
-# asdf: version manager (https://asdf-vm.com/guide/getting-started.html)
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-. $HOME/.asdf/asdf.sh && . $HOME/.asdf/completions/asdf.bash
