e72b1c46beee7f76bbe01ef97317576c5cefb61e
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2024-10-12 13:44:05 -0300

    Update pip_pipx.sh

--- install/tools/pip_pipx.sh
+++ install/tools/pip_pipx.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # install pip, pipdeptree and pip-autoremove
-sudo apt install python3-pip -y
+python3 -m ensurepip --upgrade
 sudo pip install pipdeptree
 sudo pip install pip-autoremove
 
 # install pipx
 # https://pypa.github.io/pipx/installation/
-python3 -m pip install --user pipx
-python3 -m pipx ensurepath --force
-
-# need to install the python3-venv package to use pipx
-sudo apt install python3-venv -y
+sudo apt update
+sudo apt install pipx
+pipx ensurepath
