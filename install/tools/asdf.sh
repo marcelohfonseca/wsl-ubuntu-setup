df1eee32e8772ce5c1396c541506156e2a8d35d7
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2024-10-12 13:46:12 -0300

    Create asdf.sh

---
+++ install/tools/asdf.sh
@@ -0,0 +1,3 @@
+# asdf: version manager (https://asdf-vm.com/guide/getting-started.html)
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+. $HOME/.asdf/asdf.sh && . $HOME/.asdf/completions/asdf.bash
