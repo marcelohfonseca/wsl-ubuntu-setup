c7698439f458af778a475565409495b30d9b4b5d
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2024-10-13 20:27:52 -0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -13,8 +13,8 @@ INSTALL_DIR="$DOTFILES_DIR/install"
 # function to clone dotfiles
 clone_dotfiles() {
     if [ -d "$DOTFILES_DIR" ]; then
-        read -p "O diretório .dotfiles já existe. Deseja sobrescrevê-lo? (s/n) " confirm
-        if [[ "$confirm" != "s" ]]; then
+        read -p "O diretório .dotfiles já existe. Deseja sobrescrevê-lo? (y/n) " confirm
+        if [[ "$confirm" != "y" ]]; then
             exit 1
         fi
         rm -rf "$DOTFILES_DIR"
@@ -74,12 +74,10 @@ ln -s "$HOME/.dotfiles/dotfiles/zsh/.p10k.zsh" "$HOME/.p10k.zsh"
 
 # reload new settings
 echo ""
-echo "-> Carregando perfil do zsh..."
-exec zsh
-source ~/.zshrc || { echo "Falha ao tentar aplicar as mudanças em ~/.zshrc. Reinicie seu terminal."; }
-
-# make it your default shell
+echo "-> Alterando terminal padrão..."
 chsh -s $(which zsh) $USER
+
+#source ~/.zshrc || { echo "Falha ao tentar aplicar as mudanças em ~/.zshrc. Reinicie seu terminal."; }
 
 echo ""
 echo "-> Configuração do ambiente finalizada!"
