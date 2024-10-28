a0ca910f8671d8aee222b8ac5e3fc9e6ff703ab1
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2024-10-27 21:31:06 -0300

    Update asdf.sh

--- install/tools/asdf.sh
+++ install/tools/asdf.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # asdf: version manager (https://asdf-vm.com/guide/getting-started.html)
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 . $HOME/.asdf/asdf.sh && . $HOME/.asdf/completions/asdf.bash
