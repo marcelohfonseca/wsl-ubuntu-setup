6a88765f86c12bdc73590113b465db300150c051
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2024-11-06 07:53:43 -0300

    add portuguese lang libraries-tex.sh

--- install/libraries-tex.sh
+++ install/libraries-tex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt install -y texlive texlive-latex-extra texlive-science texlive-extra-utils latexmk
+sudo apt install -y texlive texlive-latex-extra texlive-science texlive-extra-utils latexmk texlive-lang-portuguese
