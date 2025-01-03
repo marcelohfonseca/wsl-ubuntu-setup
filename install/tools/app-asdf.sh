a99b02153ff11108c3fbcde4a9b958ec832cfc91
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2025-01-03 17:26:50 -0300

    Update app-asdf.sh

--- install/tools/app-asdf.sh
+++ install/tools/app-asdf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# asdf: version manager (https://asdf-vm.com/guide/getting-started.html)
+# asdf: version manager (https://asdf-vm.com/)
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf
-. $HOME/.asdf/asdf.sh && . $HOME/.asdf/completions/asdf.bash
+. "$HOME/.asdf/asdf.sh"
